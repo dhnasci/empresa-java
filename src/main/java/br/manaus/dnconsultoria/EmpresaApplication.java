8a171a6c5de06b5e5ca0efe4894f6341e4f08031
Dirceu Henrique <dhnasci@hotmail.com>
2021-04-02 20:03:50 -0400

    Adjust for war deploy

--- src/main/java/br/manaus/dnconsultoria/EmpresaApplication.java
+++ src/main/java/br/manaus/dnconsultoria/EmpresaApplication.java
@@ -2,9 +2,17 @@ package br.manaus.dnconsultoria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmpresaApplication {
+public class EmpresaApplication extends SpringBootServletInitializer  {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return super.configure(builder);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmpresaApplication.class, args);
